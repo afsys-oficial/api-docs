49821acf072b798a4012abf28a9b799daedb336c
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2021-10-26 18:42:35 -0300

    fix: updated docs

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 ./sdeploy.sh -v
 git checkout gh-pages
 
-mv build/stylesheets/print build/stylesheets/print.css
-mv build/stylesheets/screen build/stylesheets/screen.css
+mv stylesheets/print stylesheets/print.css
+mv stylesheets/screen stylesheets/screen.css
 
 rsync -avzh --delete --exclude '.git' ./ docker@app.afsys.com.br:/home/docker/ruby/afsys_production/apidoc
 git checkout master
