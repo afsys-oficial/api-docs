8f702f78d9972b339ca7bee9f893d3c2b55a021b
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2021-10-26 18:46:32 -0300

    fix: updated docs

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,5 @@ mv stylesheets/screen stylesheets/screen.css
 
 rsync -avzh --delete --exclude '.git' ./ docker@app.afsys.com.br:/home/docker/ruby/afsys_production/apidoc
 git checkout master
+
+rm -rf stylesheets
